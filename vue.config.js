f4098ffb8707b0b642a2f2b6472fe3fc978a5810
木亘 <8119755+wooden-joint@user.noreply.gitee.com>
2022-07-07 16:46:53 +0800

    修改地址

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
       // 配置跨域，请求后端接口
       '/api': {
         // target: 'http://localhost:3030',//源地址
-        target: 'http://120.77.241.208:3030',//源地址
+        target: 'http://codeape.site:3030',//源地址
         changeOrigin: true,//是否允许跨域
         pathRewrite: {
           '^/api': '/'
@@ -13,7 +13,7 @@ module.exports = {
       },
       '/uploadapi': {
         // target: 'http://localhost:9705',//源地址
-        target: 'http://120.77.241.208:9705',//源地址
+        target: 'http://codeape.site:9705',//源地址
         changeOrigin: true,//是否允许跨域
         pathRewrite: {
           '^/uploadapi': '/'
